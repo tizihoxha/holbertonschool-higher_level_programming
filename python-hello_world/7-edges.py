78a6347a25c232d49ccfc3e07464782703ba631f
tizihoxha <tizihoxha@gmail.com>
2022-09-05 04:50:28 -0700

    added

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = [0:4]
+word_last_2 = [-2:]
+middle_word = [4]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
