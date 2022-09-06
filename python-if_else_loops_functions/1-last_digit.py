fba218cf33258b44eaa5b2198e26d7f3b79bd4c3
tizihoxha <tizihoxha@gmail.com>
2022-09-06 03:47:01 -0700

    added

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-if last_digit > 5:
-    print(F"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(F"Last digit of {number} is {last_digit} and is 0")
-elif last_digit < 6 and last_digit != 0:
-    print(F"Last digit of {number} is {last_digit} and is less than 6 and not 0")
-
+if number >= 0:
+    equi = number % 10
+else:
+    equi = abs(number) % 10 * -1
+if equi > 5:
+    print(F"Last digit of {number} is {equi} and is greater than 5")
+elif equi == 0:
+    print(F"Last digit of {number} is {equi} and is 0")
+elif equi < 6 and equi != 0:
+    print(F"Last digit of {number} is {equi} and is less than 6 and not 0")
