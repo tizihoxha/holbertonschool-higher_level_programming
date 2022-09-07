49ceeb76b59f7736e9e1f94a2e0aeabe3b175872
tizihoxha <tizihoxha@gmail.com>
2022-09-07 03:53:51 -0700

    added

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number = abs(number % 10)
+    if number >= 0:
+        number = number % 10
+    else:
+        number = abs(number % 10)
     print(F"{number}", end="")
     return (number)
