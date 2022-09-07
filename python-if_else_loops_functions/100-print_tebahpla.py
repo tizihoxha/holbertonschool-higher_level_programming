0a79a6c44a3826d7784732b3d19e46eff7c2596e
tizihoxha <tizihoxha@gmail.com>
2022-09-07 05:28:12 -0700

    added

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for c in range(ord("z"), ord("a") -1, -1):
+for c in range(ord("z"), ord("a") - 1, - 1):
     if c % 2 == 1:
         c = c - 32
     print("{:c}".format(c), end="")
