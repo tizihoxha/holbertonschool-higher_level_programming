69a3f1d67c1307a883115ed256078f22f9a3da55
tizihoxha <tizihoxha@gmail.com>
2022-09-09 02:21:51 -0700

    added

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif count == 1:
         print("1 argument:")
     else:
-        print("{} aguments:".format(count))
+        print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, argv[i + 1]))
