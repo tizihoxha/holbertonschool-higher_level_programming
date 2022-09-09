6e95abaa98b42211752b08c7094b4808dd85c5e0
tizihoxha <tizihoxha@gmail.com>
2022-09-09 02:46:23 -0700

    added

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
+    i = 1
     total = 0
-    for i in range(len(argv) - 1):
-        total = total + int(argv[i + 1]
+    while i != (len(argv)):
+        total = total + int(argv[i])
+        i = i + 1
     print("{}".format(total))
