c8a3695506ff3fe00dd02c71859afd98ebbbd27e
tizihoxha <tizihoxha@gmail.com>
2022-09-09 03:05:29 -0700

    added

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    mane_list = dir(hidden_4)
-    for name_list in names:
+    name_list = dir(hidden_4)
+    for name in name_list:
         if name[:2] != "__":
-            print(name_list)
+            print(name)
