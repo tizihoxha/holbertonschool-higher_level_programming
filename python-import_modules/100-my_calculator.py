f1500fb39b9359c004de05e2657e11a407dd9395
tizihoxha <tizihoxha@gmail.com>
2022-09-09 13:22:33 -0700

    added

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+    if operator == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif operator == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif operator == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif operator == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
