eba0f2618c63e027c9a16ffe2b18537a838097d9
tizihoxha <tizihoxha@gmail.com>
2022-09-12 03:56:50 -0700

    added

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = (''.join([my_string[i] for i in range(len(my_string) if i != c and i != C)
+    new_string = [i for i in my_string if i != 'c' and i != 'C']
+    return ("".join(new_string))
