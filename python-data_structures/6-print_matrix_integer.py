e65ef1bb83d6051cbcbf8327c84f007b087cce8d
tizihoxha <tizihoxha@gmail.com>
2022-09-12 04:11:08 -0700

    added

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" " if element != row[-1] else "")
+        for elem in row:
+            print("{:d}".format(elem), end=" " if elem != row[-1] else "")
         print()
