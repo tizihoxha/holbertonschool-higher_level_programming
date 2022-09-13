d5a8b346698ef23147865bd1550c7107ed104e6a
tizihoxha <tizihoxha@gmail.com>
2022-09-13 06:06:04 -0700

    added

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,4 +3,4 @@ def best_score(a_dictionary):
     if elem not in a_dictionary:
         return (None)
     else:
-        return (max(a_dictionary, key=lambda elem: elem[1])
+        return (max(a_dictionary, key=a_dictionary.get))
