17bfec4680ef365fa0b1c0dd1feae2ee8261ef19
tizihoxha <tizihoxha@gmail.com>
2022-09-13 03:39:14 -0700

    added

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_1 = set(a)
-    set_2 = set(b)
-    return (set(a) & set(b))
+    set_1 = set(set_1)
+    set_2 = set(set_2)
+    return (set(set_1) & set(set_2))
