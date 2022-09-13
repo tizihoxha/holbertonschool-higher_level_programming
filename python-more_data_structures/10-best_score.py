fddd22fca1ef2a4e73c301c7045c1d9ed85f6bdb
tizihoxha <tizihoxha@gmail.com>
2022-09-13 06:04:25 -0700

    added

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    keymax = max(zip(a_dictionary.values(), a_dictionary.keys())))[1]
-print (keymax)
-if keymax not in a_dictionary:
-    return (None)
+    if elem not in a_dictionary:
+        return (None)
+    else:
+        return (max(a_dictionary, key=lambda elem: elem[1])
