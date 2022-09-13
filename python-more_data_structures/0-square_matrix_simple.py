173dd6de9472dceaea1d153e2f6143a38d89c044
tizihoxha <tizihoxha@gmail.com>
2022-09-13 02:46:11 -0700

    added

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    for elem in square_matrix_simple:
+        squares_matrix-simple.append(elem**2)
+        new_matrix = len(square-matrix_simple)
+    return (new_matrix)
