8a65387b8b97bde8717b7e1a767ca93c9f8c34ec
tizihoxha <tizihoxha@gmail.com>
2022-09-13 03:22:51 -0700

    added

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for search, elem in enumerate(my_list):
+    for elem in (my_list):
         return [replace if search == elem else elem]
