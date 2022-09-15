5f7fd8c43504d8e26263f5f97193564d6d7ab1a2
tizihoxha <tizihoxha@gmail.com>
2022-09-15 04:30:26 -0700

    added

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,4 +6,4 @@ def safe_print_integer_err(value):
         return (True)
     except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (false)
+        return (False)
