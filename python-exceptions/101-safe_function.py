a1f4bb3c76ca1db2667f2e0e6f1783636b5cbd60
tizihoxha <tizihoxha@gmail.com>
2022-09-15 04:45:41 -0700

    added

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         res = fct(*args)
         return (res)
-    except (TypeError, ValueError):
+    except Exception as err:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
