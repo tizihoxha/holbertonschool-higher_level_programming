2c1ca421d77a357a9f4131e527909ef32af3f700
tizihoxha <tizihoxha@gmail.com>
2022-09-15 02:13:57 -0700

    added

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    for i in range(x):
+    for num in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             i = i + 1
