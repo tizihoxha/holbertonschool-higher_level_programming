a180c14f9ea4155dee2b3ab21d07e7f55d391b18
tizihoxha <tizihoxha@gmail.com>
2022-09-15 02:22:57 -0700

    added

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    i = 0
+    for num in range(0,x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            i = i + 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (i)
