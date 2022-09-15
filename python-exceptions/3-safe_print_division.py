c23d030adac7665bb131cf384082fd6137582326
tizihoxha <tizihoxha@gmail.com>
2022-09-15 02:58:07 -0700

    added

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,9 +3,7 @@ def safe_print_division(a, b):
     try:
         res = a / b
     except (ZeroDivisionError):
-        return (None)
-    else:
-        print("{}".format(res), end="")
+    return (None)
     finally:
         print("Inside result: {}".format(res))
     return (res)
