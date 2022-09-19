f5ea1095681b6d3604a3ac5a83ae5ad12ab74706
tizihoxha <tizihoxha@gmail.com>
2022-09-19 06:18:50 -0700

    added

---
+++ python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Task 5"""
+
+
+class Square:
+    """Write a class Square that defines a square by: (based on 0-square.py)"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size**2)
+
+    def my_print(self):
+        for i in range(self.__size):
+            print("#", end="")
+            for j in range(self.__size)
+            print("")
+            if self.__size == 0:
+                print("")
