f93d3274bc0db099a86144ca779b4c4298483252
tizihoxha <tizihoxha@gmail.com>
2022-09-19 04:12:57 -0700

    addded

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,5 +4,5 @@
 
 class Square:
     """Write a class Square that defines a square by: (based on 0-square.py)"""
-    def __init__(self, size)
-    self.__size = size
+    def __init__(self, size):
+        self.__size = size
