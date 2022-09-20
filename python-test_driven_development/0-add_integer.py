e2a6187001431f550b2aacf21e60ab115a711fb5
tizihoxha <tizihoxha@gmail.com>
2022-09-20 03:56:58 -0700

    added

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Task 0"""
+
+
+def add_integer(a, b=98):
+    """function that adds 2 integers"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
