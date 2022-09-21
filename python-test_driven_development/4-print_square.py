b3da43135f18a51f8a901bd85018d4225df38076
tizihoxha <tizihoxha@gmail.com>
2022-09-21 02:03:37 -0700

    added

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Task 3"""
+
+
+def print_square(size):
+    """function that prints a square with the character #"""
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size is float and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
