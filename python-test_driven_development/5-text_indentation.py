9c2459cd4494ccd147bbe940c6597c0c77cefd0d
tizihoxha <tizihoxha@gmail.com>
2022-09-21 02:49:43 -0700

    added

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Task 4"""
+
+
+def text_indentation(text):
+    """function that prints a text with 2 new lines after each of ., ? and :"""
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    s = [".", "?", ":"]
+    new_string = ""
+    for i in range(len(text)):
+        if text[i] == " " and flag == True:
+            continue
+        flag = False
+        new_string += text[i]
+        if text[i] in s:
+            new_string += "\n" + "\n"
+            flag = True
+    print(new_string, end="")
