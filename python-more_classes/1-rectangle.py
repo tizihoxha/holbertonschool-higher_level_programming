df17938231159d3ebc4a0360e498dc033f131aa4
tizihoxha <tizihoxha@gmail.com>
2022-09-22 03:29:15 -0700

    added

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,27 +9,27 @@ def __init__(self, width =0, height=0):
     self.width = width
     self.height = height
 
-    @property:
-        def width(self):
-            return (self.__width)
+    @property
+    def width(self):
+        return (self.__width)
 
-    @width.setter:
-        def width(self, value):
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-    @property:
-        def height(self):
-            return (self.__height)
+    @property
+    def height(self):
+        return (self.__height)
 
 
-    @height.setter:
-        def height(self, value):
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            elif value < 0:
-                raise ValueError("Height must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("Height must be >= 0")
+        self.__height = value
