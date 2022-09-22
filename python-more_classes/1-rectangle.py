96f2c72258300f2a5d54a95fd3029130c51f0046
tizihoxha <tizihoxha@gmail.com>
2022-09-22 03:14:05 -0700

    added

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,6 +18,7 @@ def __init__(self, width =0, height=0):
                 raise TypeError("width must be an integer")
             if width < 0:
                 raise ValueError("width must be >= 0")
+            self.__width = value
     @property:
         def height(self):
             return (self.__height)
@@ -29,3 +30,4 @@ def __init__(self, width =0, height=0):
                 raise TypeError("height must be an integer")
             if height < 0:
                 raise ValueError("Height must be >= 0")
+            self.__height = value
