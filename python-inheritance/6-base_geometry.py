d1d3607482541dcc5576d970c10f83b209f26467
tizihoxha <tizihoxha@gmail.com>
2022-09-27 05:17:37 -0700

    added

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Task 6"""
+
+
+class BaseGeometry:
+    """public instance method: def area(self): that raises an Exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
