41addb31d267a0d290ed0b5e3e441f5a7ad9a4d6
tizihoxha <tizihoxha@gmail.com>
2022-09-27 03:29:18 -0700

    added

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,5 +3,5 @@
 
 
 def lookup(obj):
-    """function that returns the list of available attributes and methods of an object"""
+    """function that returns the list of attr and methods of an object"""
     return(dir(obj))
