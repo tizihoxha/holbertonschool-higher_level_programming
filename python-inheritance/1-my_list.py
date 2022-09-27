6303c8b324e02d02d94c0d2c9cf855980bd0ce05
tizihoxha <tizihoxha@gmail.com>
2022-09-27 04:12:09 -0700

    added

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 """Task 1"""
 
 
-Class MyList(list):
+class MyList(list):
     """fuction that prints the list, but sorted ascending sort"""
     def print_sorted(self):
         print(sorted(self))
