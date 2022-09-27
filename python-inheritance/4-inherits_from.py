fcaba80c8ad687f0e154e0f8294330a407459296
tizihoxha <tizihoxha@gmail.com>
2022-09-27 04:58:41 -0700

    added

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Task 4"""
+
+
+def inherits_from(obj, a_class):
+    """True if the object is an instance of a class that inherited"""
+    if issubclass(type(obj), a_class):
+        return (True)
+    else:
+        return (False)
