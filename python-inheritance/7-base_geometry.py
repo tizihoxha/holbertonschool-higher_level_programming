f743dd0707bccbae99e38034109a48ea89efe66b
tizihoxha <tizihoxha@gmail.com>
2022-09-27 05:39:19 -0700

    added

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Task 6"""
+
+
+class BaseGeometry:
+    """public instance method: def area(self): that raises an Exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    if type(value) is not int:
+        raise TypeError(F"{} must be an integer"(name))
+    if value <= 0:
+        raise ValueError(F"{} must be greater than 0"(name))
