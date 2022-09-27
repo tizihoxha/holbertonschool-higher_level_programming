20a9717392175cecf8696a9cf890f20018db9420
tizihoxha <tizihoxha@gmail.com>
2022-09-27 04:53:17 -0700

    added

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Task 3"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns True if the object is an instance of"""
+    if isinstance(obj, a_class):
+        return (True)
+    else:
+        return (False)
