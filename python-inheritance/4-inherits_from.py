bd85531bb70a4425298b3a07ddec015ee39d7778
tizihoxha <tizihoxha@gmail.com>
2022-09-27 05:06:07 -0700

    added

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """True if the object is an instance of a class that inherited"""
-    if issubclass(type(obj), a_class or type(obj) is a_class):
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return (True)
     else:
         return (False)
