eb1bcc0bb9290322726471dcb451d07fc184a405
tizihoxha <tizihoxha@gmail.com>
2022-09-28 03:26:27 -0700

    added

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -5,6 +5,6 @@
 class Myint(int):
     """class MyInt that inherits from int"""
     def __eq__(self, other):
-        return (sef.value = other.value)
+        return (sef.value == other.value)
     def __ne__(self, other):
         return (not self == other)
