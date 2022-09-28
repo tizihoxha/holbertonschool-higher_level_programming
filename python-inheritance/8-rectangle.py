44b8de69b42756f28860c2a5ec245e3bf475531a
tizihoxha <tizihoxha@gmail.com>
2022-09-28 02:01:34 -0700

    added

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Task 8"""
+
+
+class BaseGeometry:
+    """public instance method: def area(self): that raises an Exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(F"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(F"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
