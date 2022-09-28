ecd8bbaf134df8bc0743f8390b0b92f784b76f3a
tizihoxha <tizihoxha@gmail.com>
2022-09-28 01:52:11 -0700

    added

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,6 +6,7 @@ class BaseGeometry:
     """public instance method: def area(self): that raises an Exception"""
     def area(self):
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         if type(value) is not int:
             raise TypeError(F"{name} must be an integer")
