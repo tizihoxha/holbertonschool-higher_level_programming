d87a65316da08de1bb92d7da647e006311798885
tizihoxha <tizihoxha@gmail.com>
2022-09-29 03:39:52 -0700

    added

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Task 2"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """function that appends a string at the end of a text file"""
     with open(filename, 'a', encoding="utf-8") as f:
         return (f.append(text))
