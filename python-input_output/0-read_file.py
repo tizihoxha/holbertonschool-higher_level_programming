a43ec03499d6cd0ec0c4f3b6e00f1fd849e82ee0
tizihoxha <tizihoxha@gmail.com>
2022-09-29 03:15:37 -0700

    added

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, 'r', encoding="utf-8") as f:
         read_file = f.read()
-        print(read_file(f))
+        print(read_file, end="")
