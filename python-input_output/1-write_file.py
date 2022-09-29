06cfcf1b0685346a103852e2e2399325ca5be35a
tizihoxha <tizihoxha@gmail.com>
2022-09-29 03:32:15 -0700

    added

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,8 +4,5 @@
 
 def write_file(filename="", text=""):
     """writes a string to a text file returns the number of characters"""
-    nr_lines = 0
-    with open(filename, 'r', encoding="utf-8") as f:
-        for nr_lines in f:
-            nr_lines = nr_lines + 1
-        print(nr_lines)
+    with open(filename, 'r+', encoding="utf-8")
+    return (f.write(text))
