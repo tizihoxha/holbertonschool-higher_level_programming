780f6090a1ea50a5f8510e8e3dfb24d74297e584
tizihoxha <tizihoxha@gmail.com>
2022-09-30 03:59:48 -0700

    added

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Task 12"""
+
+
+def pascal_triangle(n):
+    """pascal triangle"""
+    new_list = []
+    if n <= 0:
+        return new_list = []
+
+    for i in range(n):
+        row = [1]
+        if new_list:
+            last_row = new_list[-1]
+            new_list.extened([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
+        new_list.append(row)
+    return new_list
