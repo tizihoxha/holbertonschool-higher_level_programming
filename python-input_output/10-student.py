6eaae293edc0803e046c169f7dcc67a2bbbd0a40
tizihoxha <tizihoxha@gmail.com>
2022-09-30 03:29:10 -0700

    added

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -5,13 +5,15 @@
 class Student:
     """student class"""
     def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__age = age
 
     def to_json(self, attrs=None):
+        if type(attrs) is list:
+            new_dict = {}
+            for el in attrs:
+                if el in self.__dict__:
+                    new_dict[el] = self.__dict__[el]
+            return new_dict
         return (self.__dict__)
-    if type(attrs) is a list of str:
-        getattr(self.first_name, self.last_name)
-    else:
-        getattr(self.first_name, self.last_name, age))
