3a651b17895f7a731bb322f23b7c9ebf19d72112
tizihoxha <tizihoxha@gmail.com>
2022-10-03 02:45:42 -0700

    added

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr.bin/python3
+"""Task 1"""
+
+
+class Base:
+    """class Base created"""
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
