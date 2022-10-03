fa00cfbc94c4c30becf7eb3add2af86508a21a08
tizihoxha <tizihoxha@gmail.com>
2022-10-03 04:19:51 -0700

    added

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,3 +87,7 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print("")
+
+    def __str__(self):
+        """__str__ method so that it returns [Rectangle]"""
+        return (F"[Rectangle] ({self.id}) {self.__x}/{self.__y} {self.__width}/{self.__height}")
