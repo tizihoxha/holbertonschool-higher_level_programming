6693a3ccffc184e59a4b214e64a7ede8d5e2b93d
tizihoxha <tizihoxha@gmail.com>
2022-10-03 03:08:52 -0700

    added

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Task 2"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle created"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return (self.__width)
+    
+    @property
+    def height(self):
+        return (self.__height)
+
+    @property
+    def x(self):
+        return (self.__x)
+
+    @property
+    def y(self):
+        return (self.__y)
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
