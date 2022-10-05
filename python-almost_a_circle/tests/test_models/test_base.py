25d834cd93fb3046730700eef96fd4f85bb35887
tizihoxha <tizihoxha@gmail.com>
2022-10-05 04:30:41 -0700

    added

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,30 +6,33 @@ from models.base import Base
 
 class BaseTest(unittest.TestCase):
     """Test class"""
-    def id_none(self):
+    def test_id_none(self):
+        """id is none"""
         inst = Base()
         self.assertEqual(1, inst.id)
 
     def test_id(self):
+        """test id"""
         inst = Base(15)
         self.assertEqual(15, inst.id)
 
-    def negative_id(self):
+    def test_negative_id(self):
+        """negative id"""
         inst = Base(-4)
         self.assertEqual(-4, inst.id)
 
-    def zero_id(self):
+    def test_zero_id(self):
         inst = Base(0)
         self.assertEqual(0, inst.id)
 
-    def id_is_string(self):
+    def test_id_is_string(self):
         inst = Base("Hello")
         self.assertEqual("Hello", inst.id)
 
-    def id_is_list(self):
+    def test_id_is_list(self):
         inst = Base([1, 2, 3])
         self.assertEqual([1, 2, 3], inst.id)
 
-    def id_is_tuple(self):
+    def test_id_is_tuple(self):
         inst = Base((1, ))
         self.assertEqual((1, ), inst.id)
