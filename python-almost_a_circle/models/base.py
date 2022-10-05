d4d0b079921236726483f885ed957e01f686245c
tizihoxha <tizihoxha@gmail.com>
2022-10-05 02:47:02 -0700

    added

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -45,7 +45,10 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns an instance"""
-        dummy = cls(1, 1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
 
