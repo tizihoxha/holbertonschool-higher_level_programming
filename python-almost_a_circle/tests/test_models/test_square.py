7e4675cee6a37bf9c56a0a7ddb426cb41ec420ca
tizihoxha <tizihoxha@gmail.com>
2022-10-07 01:36:04 -0700

    added

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -7,7 +7,7 @@ class TestSquare(unittest.TestCase):
     """Test class for square"""
     def test_square_one_arg(self):
         square1 = Square(1)
-        self.assertEqual(square1.id, 11)
+        self.assertEqual(square1.size, 1)
 
     def test_square_two_args(self):
         square1 = Square(1, 3)
@@ -78,3 +78,44 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(1, square1.size)
         self.assertEqual(3, square1.x)
         self.assertEqual(4, square1.y)
+
+    def test_to_dict(self):
+        square1 = Square(4, 2, 3, 11)
+        dict1 = {
+                "size": 4,
+                "x": 2,
+                "y": 3,
+                "id": 11
+                }
+        self.assertEqual(dict1, square1.to_dictionary())
+
+    def test_create_dict(self):
+        dict1 = {
+                "size": 4,
+                "x": 2,
+                "y": 3,
+                "id": 11
+                }
+        square1 = Square.create(**dict1)
+
+    def test_save_to_file(self):
+        Square.save_to_file([Square(1)])
+        with open("Square.json", "r", encoding="utf-8") as f:
+            string = f.read()
+            self.assertEqual(str, type(string))
+
+    def test_save_to_empty_file(self):
+        Square.save_to_file([])
+        with open("Square.json", "r", encoding="utf-8") as f:
+            self.assertEqual("[]", f.read())
+
+    def test_save_to_file_none(self):
+        Square.save_to_file(None)
+        with open("Square.json", "r", encoding="utf-8") as f:
+            self.assertEqual("[]", f.read())
+
+    def test_load_from_file(self):
+        square1 = Square(4, 2, 3, 11)
+        Square.save_to_file([square1])
+        sqlist = Square.load_from_file()
+        self.assertEqual(str(square1), str(sqlist[0]))
