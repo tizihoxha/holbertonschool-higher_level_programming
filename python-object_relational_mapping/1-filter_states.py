294c6200ac4021a9334a9b34bcdcf9eedb42a5ba
tizihoxha <tizihoxha@gmail.com>
2022-11-09 05:17:13 -0800

    added

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0"""
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id WHERE  name= 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
