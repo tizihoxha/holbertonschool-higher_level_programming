1f983419c422e055ce3b8ad8ce55632e1944ce9f
tizihoxha <tizihoxha@gmail.com>
2022-11-09 01:26:52 -0800

    added

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- shows show shws with genre id NULL.
-SELECT tv_shows.tittle, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 Where genre_id IS NULL
