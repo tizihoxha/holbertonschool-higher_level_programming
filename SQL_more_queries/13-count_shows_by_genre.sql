be8eb4de4a31e5fba616510408a9f87276142897
tizihoxha <tizihoxha@gmail.com>
2022-11-09 01:45:19 -0800

    added

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre, number_shows
+GROUP BY tv_genres.name, tv_show_genres.genre_id
 ORDER BY number_shows DESC;
