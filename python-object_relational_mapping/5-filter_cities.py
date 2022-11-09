7813a3ca229b415893c409ec40ff613754ae460c
tizihoxha <tizihoxha@gmail.com>
2022-11-09 06:06:57 -0800

    added

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states"""
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities WHERE cities.state_id=(SELECT id\
+              FROM states WHERE name='{}') ORDER BY cities.id".format(state,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
