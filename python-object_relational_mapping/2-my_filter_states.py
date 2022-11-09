66dec08005dc6e348f4bc2935507580b6a9b7402
tizihoxha <tizihoxha@gmail.com>
2022-11-09 05:36:57 -0800

    added

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that lists all states with a name start with N from the database"""
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY '{}'"
+                "ORDER BY states.id".format(argv[4])
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
