5939c0e584c49f6e5249f13255b0f3b6991ca027
tizihoxha <tizihoxha@gmail.com>
2022-11-09 05:54:21 -0800

    added

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY %s"
-                "ORDER BY states.id"(argv[4], ))
+                "ORDER BY states.id", (argv[4], ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
