5cae1c2e272fd62d97911af39dc6f8100b8cce35
tizihoxha <tizihoxha@gmail.com>
2022-11-09 01:28:35 -0800

    added

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 Where genre_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genres_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
