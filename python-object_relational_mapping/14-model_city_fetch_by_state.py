e8849d6373d1f99933d006f9900026b7e9a67fdc
tizihoxha <tizihoxha@gmail.com>
2022-11-10 07:01:24 -0800

    added

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine(F"mysql+mysqldb://{argv[1]}:{argv[2]}\
+                            @localhost/{argv[3]}")
+    with Session(engine) as session:
+        cities = session.query(State, City).\
+                filter(State.id == City.state_id).order_by(City.id)
+                for state, city in cities:
+                    print(F"{state.name}: ({city.id}) {city.name}")
---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""python file that contains the class definition of a State and an
+instance Base = declarative_base()"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """state class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id", nullable=False)
