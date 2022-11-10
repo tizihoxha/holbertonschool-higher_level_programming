b416b0f46a5a7c27829df439bedddfeca0e73123
tizihoxha <tizihoxha@gmail.com>
2022-11-10 06:42:54 -0800

    added

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine(F"mysql+mysqldb://{argv[1]}:{argv[2]}\
+                            @localhost/{argv[3]}")
+    with Session(engine) as session:
+        new_state = State(name="Louisiana")
+        session.add(new_state)
+        session.commit()
+            print(F"{new_state.id}")
