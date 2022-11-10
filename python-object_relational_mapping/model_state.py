d86bb55dbbcfdae583ff7adb92d66779191b3433
tizihoxha <tizihoxha@gmail.com>
2022-11-10 03:49:09 -0800

    Added

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""python file that contains the class definition of a State and an instance Base = declarative_base()"""
+"""python file that contains the class definition of a State and an
+instance Base = declarative_base()"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
