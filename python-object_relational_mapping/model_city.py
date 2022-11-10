6a489b58e845324eeda071837f5e8ccd98303f30
tizihoxha <tizihoxha@gmail.com>
2022-11-10 07:14:02 -0800

    Task 14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""python file that contains the class definition of a State and an
-instance Base = declarative_base()"""
+"""contains the class defininition of a city that maps to database"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
 
 
-class State(Base):
-    """state class"""
+class City(Base):
+    """City class that maps to cities table"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
