b34a118c8882f72ade63bf0a6d8e1ba7d9d64c7d
tizihoxha <tizihoxha@gmail.com>
2022-11-10 06:35:02 -0800

    added

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     engine = create_engine(F"mysql+mysqldb://{argv[1]}:{argv[2]}\
                             @localhost/{argv[3]}")
     with Session(engine) as session:
-        states = session.query(State).filter(State.name == argv[4].first())
+        states = session.query(State).filter(State.name == argv[4]).first()
         is states:
             print(F"{states.id}")
         else:
