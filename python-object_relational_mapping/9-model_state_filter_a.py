cc4f7069a623eeb251403c7c6c236cc41b8182dd
tizihoxha <tizihoxha@gmail.com>
2022-11-10 06:26:43 -0800

    added

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine(F"mysql+mysqldb://{argv[1]}:{argv[2]}\
+                            @localhost/{argv[3]}")
+    with Session(engine) as session:
+        states = session.query(State).filter(State.name.like("%a%"))
+        for state in states:
+            print(F"{state.id}: {state.name}")
