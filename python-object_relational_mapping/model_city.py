ba1218d6eab82ba92cde5ff3603471c97dfe7a9c
tizihoxha <tizihoxha@gmail.com>
2022-11-10 07:04:36 -0800

    added

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -14,4 +14,4 @@ class State(Base):
     __tablename__ = 'cities'
     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id", nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
