147192cecab9f7c3ad78760558cf36fb9d3b79ea
tizihoxha <tizihoxha@gmail.com>
2022-12-07 05:07:49 -0800

    added

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -3,4 +3,4 @@
  script that prints two arguments passed to it, in the following format:“is”
  */
 
-console.log(process.argv[2] + 'is' + process.argv[3]);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
