d99b35accd0cf1719c436c38405199f692a843f7
tizihoxha <tizihoxha@gmail.com>
2022-12-07 04:37:39 -0800

    added

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ script that prints 3 lines
+ */
+
+const myVar = 'C is fun';
+const myvar = 'Python is cool';
+const Myvar = 'JavaScript is amazing';
+
+console.log(myVar);
+console.log(myvar);
+console.log(Myvar);
