306ef30a24d46f97ea38db19b8ad8c1e9e6da11e
tizihoxha <tizihoxha@gmail.com>
2022-12-07 05:22:02 -0800

    added

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
  script that prints My number: <first argument converted in integer>
  */
 
-const number = parseInt(process.aargv[2]);
+const number = parseInt(process.argv[2]);
 
 if (!number) {
   console.log('Not a number');
