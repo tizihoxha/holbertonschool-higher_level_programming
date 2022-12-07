3d2ed75c6e55f8ab50bb0254c37a6b25842e21a8
tizihoxha <tizihoxha@gmail.com>
2022-12-07 05:48:50 -0800

    added

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -7,8 +7,6 @@ const number = parseInt(process.argv[2]);
 
 if (!number) {
   console.log('Missing size');
-} else if (number < 0) {
-  console.log('');
 } else {
   for (let i = 0; i < number; i++) {
     console.log('X'.repeat(number));
