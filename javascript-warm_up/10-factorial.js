072c83204a0ff9d9dd570a599915eb4ea3e1369e
tizihoxha <tizihoxha@gmail.com>
2022-12-07 06:26:08 -0800

    added

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/*
+ Factorial
+ */
+
+function factorial (n) {
+  if (!isNaN(n)) {
+    return (n * factorial(n - 1));
+  } else {
+    return (1);
+  }
+}
+
+const number = parseInt(process.argv[2]);
+console.log(factorial(number));
