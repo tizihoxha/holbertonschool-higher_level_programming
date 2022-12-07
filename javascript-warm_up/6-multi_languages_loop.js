3301b8b2c412e26dfaa252d06e6d8a2dadbcdeb7
tizihoxha <tizihoxha@gmail.com>
2022-12-07 05:29:54 -0800

    added

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+ script that prints 3 lines
+ */
+
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
