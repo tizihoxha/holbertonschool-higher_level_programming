c4f3b522dc72ba3d3bc19a2a899dbd6b13009948
tizihoxha <tizihoxha@gmail.com>
2022-12-08 03:45:40 -0800

    added

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ Square
+ */
+
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
