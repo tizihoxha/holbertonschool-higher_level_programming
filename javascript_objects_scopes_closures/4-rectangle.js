0a7545481fe62a66f2a6b4eeb12571d0c337afed
tizihoxha <tizihoxha@gmail.com>
2022-12-08 03:33:22 -0800

    added

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -18,8 +18,9 @@ module.exports = class Rectangle {
   }
 
   rotate () {
+    const temp = this.width;
     this.width = this.height;
-    this.height = this.width;
+    this.height = temp;
   }
 
   double () {
