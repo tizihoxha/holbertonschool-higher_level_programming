73f5b72dc82f9deedc751cce594e777af1900b82
tizihoxha <tizihoxha@gmail.com>
2022-12-08 04:08:43 -0800

    added

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -4,11 +4,11 @@
  */
 
 exports.nbOccurences = function (list, searchElement) {
-	let occurence = 0;
-	for (let i = 0; i < list.length; i++) {
-		if (list[i] === searchElement) {
-			occurence++;
-		}
-	}
-	return occurence;
+  let occurence = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      occurence++;
+    }
+  }
+  return occurence;
 };
