a33a39419c13dc35b965876e0e6a6df3190d6d09
tizihoxha <tizihoxha@gmail.com>
2022-12-08 03:59:44 -0800

    added

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/*
+ Square
+ */
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
