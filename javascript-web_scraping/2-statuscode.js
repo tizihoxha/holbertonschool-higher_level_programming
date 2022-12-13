78b40db0e1c4b0e22f625c03b9d104f119632245
tizihoxha <tizihoxha@gmail.com>
2022-12-13 02:55:13 -0800

    added

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -6,7 +6,7 @@
 const request = require('request');
 
 request.get(process.argv[2], function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+  if (!error && response.statusCode) {
     console.log('code:', response.statusCode);
   }
 });
