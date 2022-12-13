7aa3668bd8e8857a576aa857a8caac85a7002834
tizihoxha <tizihoxha@gmail.com>
2022-12-13 01:42:19 -0800

    added

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ Write a script that reads and prints the content of a file
+ */
+
+var fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function(error, data) {
+	if (error) throw error;
+	console.log(data);
+});
