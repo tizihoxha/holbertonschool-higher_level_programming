8d9853bd87cafac50dbe69eda8bfa94be3b36850
tizihoxha <tizihoxha@gmail.com>
2022-12-13 01:45:05 -0800

    added

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function(error, data) {
-	if (error) throw error;
-	console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (error, data) {
+  if (error) throw error;
+  console.log(data);
 });
