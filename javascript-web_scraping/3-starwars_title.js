8035add105c2eb5dda5e6e695694be8c3e67b4bf
tizihoxha <tizihoxha@gmail.com>
2022-12-13 03:23:55 -0800

    added

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ script that prints the title of a Star Wars movie where the episode number matches a given integer
+ */
+
+const request = require('request');
+const endpoint = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
+request.get(endpoint, function (error, response, body) {
+  if (error) throw error;
+  console.log(JSON.parse(body).title);
+});
