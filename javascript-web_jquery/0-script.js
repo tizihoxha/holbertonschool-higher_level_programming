82aff517cd1a754f988d2a5b0cedc47ca1570551
tizihoxha <tizihoxha@gmail.com>
2022-12-14 04:22:48 -0800

    aded

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 /*
-JavaScript script that updates the text color of the <header>
-*/
+ * JavaScript script that updates the text color of the <header>
+ * */
 document.querySelector('header').style.color = '#FF0000';
+
